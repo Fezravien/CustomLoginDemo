cd2d56f06fb811ced48551b15429ade7dc0ecec9
pazrvien <yjaewoong@naver.com>
2020-12-11 00:35:49 +0900

    Firebase 서버에 있는 인증 정보로 로그인 하기, 홈 화면 백그라운드 비디오 재생(AVkit)

--- CustomLoginDemo/View Controllers/LoginViewController.swift
+++ CustomLoginDemo/View Controllers/LoginViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var firstNameTextField: UITextField!
-    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
@@ -26,12 +27,34 @@ class LoginViewController: UIViewController {
         errorLabel.alpha = 0
         
         // Style the elements
-        Utilities.styleTextField(firstNameTextField)
-        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(loginBtn)
     }
     
     @IBAction func loginTapped(_ sender: Any) {
+        
+        // Validate Text Fields
+        
+        // Create clleaned version
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                // Couldn't sign in
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            } else {
+                let homeViewControllers = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                
+                self.view.window?.rootViewController = homeViewControllers
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        
     }
     
    
